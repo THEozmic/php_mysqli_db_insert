5ed7b85b5ddea19513e92c0dbe9b5b6ee1137fd3
Michael Ozoemena <michaelozoemena2@gmail.com>
2016-11-20 14:09:12 +0100

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,4 +1,9 @@
 <?php
-$connect = mysqli_connect("localhost", "root", "", "qrystaldevs");
+$connect = mysqli_connect("localhost", "root", "", "dbname");
 //error_reporting(E_ALL);
+
+// localhost = db host
+// root = db username
+// "" = db password
+// dbname = db name
 ?>
